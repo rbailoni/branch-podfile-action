82f2ef88fecb99d8c0e05638c98887079a37220d
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2022-12-26 02:21:46 -0300

    Create method if file exists

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
+async function fileExists(filePath) {
+    return fs.promises.access(filePath)
+    .then(() => {
+        core.info(`File ${filePath} exists`);
+        return true;
+    })
+    .catch(() => {
+        core.setFailed(`File ${filePath} does not exist`);
+        return false;
+    });
+}
 
 (
     async () => {
         try {
-            const podPath = core.getInput('podfile-path')
-            core.notice("Validating... " + podPath);
+            core.notice("Validating Podfile... ");
+            const podPath = core.getInput('podfile-path');
+            fileExists(podPath);
         } catch (error) {
             core.setFailed(error.message);
         }
